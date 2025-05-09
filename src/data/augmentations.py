f9df82709e15e88d0e1d60239833e2c96965d009
francisco.m.ribeiro <francisco.m.ribeiro@inesctec.pt>
2025-05-09 20:11:19 +0100

    keep seg as long

--- src/data/augmentations.py
+++ src/data/augmentations.py
@@ -60,7 +60,7 @@ class JointAugment:
         # 6) ToTensor; keep seg as LongTensor mask
         #rgb   = TF.to_tensor(rgb)
         #depth = TF.to_tensor(depth)
-        #seg   = torch.from_numpy(np.array(seg)).long()
+        seg   = torch.from_numpy(np.array(seg)).long()
         return rgb, depth, seg
 
 class AugmentedDataset(Dataset):
