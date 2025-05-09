776d23d9c0a8204620d0eb3e96800582eaef93b8
francisco.m.ribeiro <francisco.m.ribeiro@inesctec.pt>
2025-05-09 16:27:10 +0100

    changed params for debug

--- scripts/train_dino.sh
+++ scripts/train_dino.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --partition=gpu_min80gb   # Partition where the job will be run. Check with "$ sinfo".
-#SBATCH --qos=gpu_min80gb        # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
+#SBATCH --partition=gpu_min32gb   # Partition where the job will be run. Check with "$ sinfo".
+#SBATCH --qos=gpu_min32gb        # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
 #SBATCH --job-name=debug_dino     # Job name
 #SBATCH --output=./logs/slurm_%x.%j.out  # File containing STDOUT output
 #SBATCH --error=./logs/slurm_%x.%j.err   # File containing STDERR output. If ommited, use STDOUT.
