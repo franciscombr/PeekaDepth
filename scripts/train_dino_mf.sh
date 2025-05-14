a19cdfab4f86bc5014b047c642be7f132fb3816d
francisco.m.ribeiro <francisco.m.ribeiro@inesctec.pt>
2025-05-14 11:43:04 +0100

    train dinov2 mid fusion

---
+++ scripts/train_dino_mf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#SBATCH --partition=gpu_min80gb   # Partition where the job will be run. Check with "$ sinfo".
+#SBATCH --qos=gpu_min80gb        # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
+#SBATCH --job-name=ft_dino     # Job name
+#SBATCH --output=./logs/slurm_%x.%j.out  # File containing STDOUT output
+#SBATCH --error=./logs/slurm_%x.%j.err   # File containing STDERR output. If ommited, use STDOUT.
+
+# Commands / scripts to run (e.g., python3 train.py)
+python3 src/train.py --config src/config/dinov2_conf_mid_fusion.yaml
